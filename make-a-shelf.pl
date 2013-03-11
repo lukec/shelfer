83faef7c2d68bb0ef0bed1e54074ecf0b72e3f3d
Luke Closs <luke@5thplane.com>
2013-03-10 23:54:03 -0700

    Refactor script to take command line params

--- make-a-shelf.pl
+++ make-a-shelf.pl
@@ -3,16 +3,28 @@ use strict;
 use warnings;
 use SVG;
 use IO::All;
+use Getopt::Long;
 
-my $output_file = "shelf.svg";
+my %p;
+GetOptions( \%p,
+    'height|h=s',
+    'width|w=s',
+    'depth|d=s',
+    'output|o=s',
+    'smudge|s=s',
+) or usage();
+
+$p{smudge} //= 1.417;
+$p{output} //= 'shelf.svg';
 
 # create an SVG object with a size of 200x200 pixels
+# XXX This seems to not export into .dxf format very well.
+# Loial says it defaults to 90dpi
 my $svg = SVG->new(
     width  => "100cm",
     height => "80cm",
-    viewBox => '0 0 1000 800',
 );
-$svg->title()->cdata('I am a title');
+$svg->title()->cdata('I am a shelf');
 
 # use explicit element constructor to generate a group element
 my $y = $svg->group(
@@ -23,20 +35,8 @@ my $y = $svg->group(
     },
 );
 
-my $inches_to_mm = sub { $_[0] * 25.4 };
-my $material_thickness = 5.5; 
-
-my ($shelf_height, $shelf_width, $shelf_depth);
-if (!"Testing mode!") {
-    $shelf_height = $inches_to_mm->(1);
-    $shelf_width  = $inches_to_mm->(3);
-    $shelf_depth  = $inches_to_mm->(3);
-}
-else {
-    $shelf_height = $inches_to_mm->(7.25);
-    $shelf_width  = $inches_to_mm->(38);
-    $shelf_depth  = $inches_to_mm->(3.5);
-}
+my $inches_to_dpi = sub { $_[0] * 90 };
+my $material_thickness = 19.5;
 
 #     25.4mm, but gap is 26.2 = 0.8mm -> 0.4mm tweak
 #    +----+
@@ -49,18 +49,16 @@ else {
 # |1        8|
 # +----------+
 
-my $smudge = 0.4;
-
-my $shoulder_height = $shelf_height - $material_thickness;
+my $shoulder_height = $p{height} - $material_thickness;
 my @support_points = (
     [0, 0], # 1
     [0, $shoulder_height], # 2
-    [($shelf_depth / 3) - $smudge, $shoulder_height], # 3
-    [($shelf_depth / 3) - $smudge, $shelf_height], # 4
-    [($shelf_depth * 2 / 3) + $smudge,, $shelf_height], # 5
-    [($shelf_depth * 2 / 3) + $smudge,, $shoulder_height], # 6
-    [$shelf_depth, $shoulder_height], # 7
-    [$shelf_depth, 0], # 8
+    [($p{depth} / 3) - $p{smudge}, $shoulder_height], # 3
+    [($p{depth} / 3) - $p{smudge}, $p{height}], # 4
+    [($p{depth} * 2 / 3) + $p{smudge}, $p{height}], # 5
+    [($p{depth} * 2 / 3) + $p{smudge}, $shoulder_height], # 6
+    [$p{depth}, $shoulder_height], # 7
+    [$p{depth}, 0], # 8
     [0,0],
 );
 
@@ -74,11 +72,11 @@ for (1 .. 3) {
         y => [ map { $y_offset + $_->[1] } @support_points ],
     );
     $y->polygon(%$points);
-    $x_offset += $shelf_depth + 10;
+    $x_offset += $p{depth} + 10;
 }
 
 $x_offset = 10;
-$y_offset = $shelf_height + 20;
+$y_offset = $p{height} + 20;
 
 # Now make the long shelf
 
@@ -95,17 +93,17 @@ $y_offset = $shelf_height + 20;
 
 my @shelf_points = (
     [ 0, 0], # 1
-    [ $shelf_width, 0], # 2
-    [ $shelf_width, $shelf_depth / 3 ], # 3
-    [ $shelf_width - $material_thickness, $shelf_depth / 3], # 4
-    [ $shelf_width - $material_thickness, $shelf_depth * 2 / 3], # 5
-    [ $shelf_width, $shelf_depth * 2 / 3], # 6
-    [ $shelf_width, $shelf_depth ], # 7
-    [ 0, $shelf_depth ], # 8
-    [ 0, $shelf_depth * 2 / 3 ], # 9
-    [ $material_thickness, $shelf_depth * 2 / 3 ], # 10
-    [ $material_thickness, $shelf_depth / 3 ], # 11
-    [ 0, $shelf_depth / 3 ], # 12
+    [ $p{width}, 0], # 2
+    [ $p{width}, $p{depth} / 3 ], # 3
+    [ $p{width} - $material_thickness, $p{depth} / 3], # 4
+    [ $p{width} - $material_thickness, $p{depth} * 2 / 3], # 5
+    [ $p{width}, $p{depth} * 2 / 3], # 6
+    [ $p{width}, $p{depth} ], # 7
+    [ 0, $p{depth} ], # 8
+    [ 0, $p{depth} * 2 / 3 ], # 9
+    [ $material_thickness, $p{depth} * 2 / 3 ], # 10
+    [ $material_thickness, $p{depth} / 3 ], # 11
+    [ 0, $p{depth} / 3 ], # 12
     [ 0, 0 ],
 );
 my $points = $svg->get_path(
@@ -116,13 +114,13 @@ my $points = $svg->get_path(
 );
 $y->polygon(%$points);
 
-my $half_way = $shelf_width / 2;
+my $half_way = $p{width} / 2;
 my @shelf_hole_points = (
-    [ $half_way - ($material_thickness / 2), $shelf_depth / 3 ], # 1
-    [ $half_way + ($material_thickness / 2), $shelf_depth / 3 ], # 2
-    [ $half_way + ($material_thickness / 2), $shelf_depth * 2 / 3 ], # 3
-    [ $half_way - ($material_thickness / 2), $shelf_depth * 2 / 3 ], # 3
-    [ $half_way - ($material_thickness / 2), $shelf_depth / 3 ], # 3
+    [ $half_way - ($material_thickness / 2), $p{depth} / 3 ], # 1
+    [ $half_way + ($material_thickness / 2), $p{depth} / 3 ], # 2
+    [ $half_way + ($material_thickness / 2), $p{depth} * 2 / 3 ], # 3
+    [ $half_way - ($material_thickness / 2), $p{depth} * 2 / 3 ], # 3
+    [ $half_way - ($material_thickness / 2), $p{depth} / 3 ], # 3
 );
 $points = $svg->get_path(
     -type => 'polygon',
@@ -132,7 +130,38 @@ $points = $svg->get_path(
 );
 $y->polygon(%$points);
 
+io($p{output})->print($svg->xmlify);
+print "Wrote to $p{output}\n";
+exit;
 
-# now render the SVG object, implicitly use svg namespace
-io($output_file)->print($svg->xmlify);
-print "Wrote to $output_file\n";
+
+
+sub usage {
+    die <<EOT }
+USAGE: $0 [-w|--width=30] [-h|--height=8] [-d|--depth=5] [-o outputfilename]
+
+  --smudge=1.5 - set this to half the width of the laser, in DPI
+
+All dimensions should be specified in inches.  I know, right? Why not metric, the author is a Canadian, too.  Disgraceful.
+EOT
+
+__DATA__
+
+if (!"Testing mode!") {
+    $p{height} = $inches_to_dpi->(1);
+    $p{width}  = $inches_to_dpi->(3);
+    $p{depth}  = $inches_to_dpi->(3);
+}
+else {
+    if (! "Tea shelf") {
+        $p{height} = $inches_to_dpi->(7.25);
+        $p{width}  = $inches_to_dpi->(38);
+        $p{depth}  = $inches_to_dpi->(3.5);
+    }
+    elsif ("Baking Shelf") {
+        $p{height} = $inches_to_dpi->(7.5);
+        $p{width}  = $inches_to_dpi->(37);
+        $p{depth}  = $inches_to_dpi->(8);
+    }
+}
+
